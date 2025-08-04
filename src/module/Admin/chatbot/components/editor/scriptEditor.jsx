f2aeb689aa421bebc87d8afa73fd0e37228d41ad
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-08-04 14:37:24 -0300

    Update scriptEditor.jsx

--- src/module/Admin/chatbot/components/editor/scriptEditor.jsx
+++ src/module/Admin/chatbot/components/editor/scriptEditor.jsx
@@ -4,7 +4,7 @@ import { EditorView, keymap, lineNumbers, highlightActiveLineGutter, highlightSp
 import { Extension, EditorSelection } from "@codemirror/state";
 import { defaultHighlightStyle, syntaxHighlighting, indentOnInput, bracketMatching, foldGutter, foldKeymap } from "@codemirror/language";
 import { defaultKeymap, history, historyKeymap } from "@codemirror/commands";
-import { searchKeymap, highlightSelectionMatches } from "@codemirror/search";
+import { searchKeymap } from "@codemirror/search";
 import { autocompletion, completionKeymap, closeBrackets, closeBracketsKeymap } from "@codemirror/autocomplete";
 import { lintKeymap } from "@codemirror/lint";
 import { javascript } from "@codemirror/lang-javascript";
