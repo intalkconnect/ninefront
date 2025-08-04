a06867475642a769bd29495b96efaf5a06cb1460
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-08-04 15:01:58 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@codemirror/state': path.resolve(__dirname, 'node_modules/@codemirror/state'),
-      '@codemirror/view': path.resolve(__dirname, 'node_modules/@codemirror/view'),
-      '@codemirror/basic-setup': path.resolve(__dirname, 'node_modules/@codemirror/basic-setup'),
-      '@codemirror/lang-javascript': path.resolve(__dirname, 'node_modules/@codemirror/lang-javascript'),
-      '@codemirror/theme-one-dark': path.resolve(__dirname, 'node_modules/@codemirror/theme-one-dark'),
-    },
-    dedupe: [
+  optimizeDeps: {
+    include: [
       '@codemirror/state',
       '@codemirror/view',
       '@codemirror/basic-setup',
@@ -20,6 +12,11 @@ export default defineConfig({
       '@codemirror/theme-one-dark',
     ],
   },
-    server: { historyApiFallback: true },
+  build: {
+    commonjsOptions: {
+      include: [/node_modules/],
+    },
+  },
+  server: { historyApiFallback: true },
   css: { modules: true },
 });
