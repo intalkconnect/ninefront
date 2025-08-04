480ff3f3632e40c6a838e6dcd25fc0e3e16389c1
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-08-03 22:25:09 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import monacoEditorPlugin from 'vite-plugin-monaco-editor';
+import { monacoEditorPlugin } from 'vite-plugin-monaco-editor';
 
 export default defineConfig({
   plugins: [
     react(),
     monacoEditorPlugin({
-      languageWorkers: ['javascript'], // ou ['editorWorkerService', 'typescript', 'json', 'html', 'css']
+      languageWorkers: ['javascript'], // ou adicione mais conforme necessário
     }),
   ],
   server: {
