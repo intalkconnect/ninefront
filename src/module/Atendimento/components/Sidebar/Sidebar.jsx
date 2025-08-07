f18f3d79585a0952e3c39a774b433c257e0c2c33
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-08-07 18:39:57 -0300

    Update Sidebar.jsx

--- src/module/Atendimento/components/Sidebar/Sidebar.jsx
+++ src/module/Atendimento/components/Sidebar/Sidebar.jsx
@@ -51,6 +51,15 @@ export default function Sidebar() {
     }
   }, [userEmail, userFilas]);
 
+  const channelIcons = {
+  whatsapp: "/icons/whatsapp.png",
+  telegram: "/icons/telegram.png",
+  instagram: "/icons/instagram.png",
+  facebook: "/icons/facebook.png",
+  messenger: "/icons/messenger.png",
+  // adicione outros canais conforme necessário
+};
+
   const puxarProximoTicket = async () => {
     try {
       const res = await apiPut("/chats/fila/proximo", {
@@ -208,7 +217,6 @@ export default function Sidebar() {
         const isSelected = fullId === selectedUserId;
         const unreadCount = unreadCounts[fullId] || 0;
         const showUnread = !isSelected && unreadCount > 0;
-        const canalWhatsapp = conv.channel === "whatsapp";
 
         return (
           <li
@@ -228,13 +236,13 @@ export default function Sidebar() {
                 >
                   {conv.name?.charAt(0).toUpperCase() || "U"}
                 </div>
-                {canalWhatsapp && (
-                  <img
-                    src="/icons/whatsapp.png"
-                    alt="whatsapp"
-                    className="channel-icon-overlay"
-                  />
-                )}
+        {conv.channel && channelIcons[conv.channel.toLowerCase()] && (
+          <img
+            src={channelIcons[conv.channel.toLowerCase()]}
+            alt={conv.channel}
+            className="channel-icon-overlay"
+          />
+        )}
               </div>
 
               <div className="chat-details">
