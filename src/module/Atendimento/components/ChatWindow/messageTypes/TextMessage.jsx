242438d0e403f44ec47b23dc239d26b233b5af7d
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-08-17 18:33:18 -0300

    Delete src directory

--- src/module/Atendimento/components/ChatWindow/messageTypes/TextMessage.jsx
+++
@@ -1,7 +0,0 @@
-// src/components/ChatWindow/messageTypes/TextMessage.jsx
-import React from 'react';
-import './TextMessage.css';
-
-export default function TextMessage({ content }) {
-  return <p className="text-message">{content}</p>;
-}
