78848d274105994dbb6bd5465be02a08c78a7d4d
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-08-25 00:55:07 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ConfirmProvider } from './components/ConfirmProvider.jsx';
 import './index.css';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ConfirmProvider>
+      <App />
+    </ConfirmProvider>
   </React.StrictMode>
 );
