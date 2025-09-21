0727039bcb43d1eb9fd4f09b3947a882fdb05534
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-09-21 16:18:11 -0300

    Update UnknownMessage.jsx

--- src/features/atendimento/components/chat/message/messageTypes/UnknownMessage.jsx
+++ src/features/atendimento/components/chat/message/messageTypes/UnknownMessage.jsx
@@ -1,7 +1,7 @@
 
 // src/components/ChatWindow/messageTypes/UnknownMessage.jsx
 import React from 'react';
-import './UnknownMessage.css';
+import './styles/UnknownMessage.css';
 
 export default function UnknownMessage() {
   return <p className="unknown-message">Tipo de mensagem desconhecido.</p>;
