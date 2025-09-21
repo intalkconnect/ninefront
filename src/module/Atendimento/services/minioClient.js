648f191262683d21f19fbc83b0690a844201737c
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-09-21 14:54:51 -0300

    Delete src directory

--- src/module/Atendimento/services/minioClient.js
+++
@@ -1,9 +0,0 @@
-import { Client } from 'minio'
-
-export const minioClient = new Client({
-  endPoint: process.env.MINIO_ENDPOINT,
-  useSSL: true,
-  accessKey: process.env.MINIO_ACCESS_KEY,
-  secretKey: process.env.MINIO_SECRET_KEY,
-  port: 443
-})
