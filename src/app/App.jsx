645caf02211c885b7a14825c102f98e8191643fb
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-09-21 18:26:07 -0300

    Update App.jsx

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,6 +1,25 @@
+// src/App.jsx
 import React from 'react';
 import AppRoutes from './routes/AppRoutes';
 
+// toastify
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 export default function App() {
-  return <AppRoutes />;
+  return (
+    <>
+      <AppRoutes />
+      <ToastContainer
+        position="top-right"
+        autoClose={2500}
+        newestOnTop
+        closeOnClick
+        pauseOnFocusLoss
+        pauseOnHover
+        draggable
+        theme="light" // ou "dark" se preferir
+      />
+    </>
+  );
 }
