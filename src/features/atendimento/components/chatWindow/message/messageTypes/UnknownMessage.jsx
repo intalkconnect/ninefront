97d761727490df49b895b09ae99f46e0dec1e4f6
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-09-21 15:16:03 -0300

    Delete src/features/atendimento/components/chatWindow directory

--- src/features/atendimento/components/chatWindow/message/messageTypes/UnknownMessage.jsx
+++
@@ -1,8 +0,0 @@
-
-// src/components/ChatWindow/messageTypes/UnknownMessage.jsx
-import React from 'react';
-import './UnknownMessage.css';
-
-export default function UnknownMessage() {
-  return <p className="unknown-message">Tipo de mensagem desconhecido.</p>;
-}
